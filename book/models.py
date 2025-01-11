41bb246f0ad37290f12540323266205155832daa
Maksym Nimych <maksimnimitch@gmail.com>
2025-01-11 20:55:57 +0200

    Add str methods to the models for better data display

--- book/models.py
+++ book/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
 class Book(models.Model):
@@ -17,7 +23,13 @@ class Book(models.Model):
     pages = models.IntegerField()
     summary = models.TextField()
 
+    def __str__(self):
+        return "Book: " + self.name
+
 
 class Chapter(models.Model):
     name = models.CharField(max_length=128)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Chapter: " + self.name
