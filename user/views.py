1d98de0bfea1d0c85db8a802cca4f867c292f757
Maksym Nimych <maksimnimitch@gmail.com>
2025-01-18 21:49:43 +0200

    Add MyLibraryView to list all the books from users library

--- user/views.py
+++ user/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user import serializers
+from book.serializers import BookListSerializer
+from book.models import Book
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -17,3 +19,16 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class MyLibraryView(generics.ListAPIView):
+    serializer_class = BookListSerializer
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (JWTAuthentication, )
+
+    def get_queryset(self):
+        queryset = Book.objects.filter(
+            users=self.request.user.id
+        ).prefetch_related("genres", "authors")
+
+        return queryset
