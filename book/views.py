8de3d56e2f827dd1f76da15e7d73d6a234ca2a1c
Maksym Nimych <maksimnimitch@gmail.com>
2025-01-18 21:51:29 +0200

    Add an action to be able to add/remove a book from the library

--- book/views.py
+++ book/views.py
@@ -1,5 +1,9 @@
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from book import models, serializers
 
@@ -24,6 +28,8 @@ class AuthorViewSet(
 
 class BookViewSet(ModelViewSet):
     queryset = models.Book.objects.prefetch_related("genres", "authors")
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (JWTAuthentication, )
 
     def get_queryset(self):
         queryset = self.queryset
@@ -39,6 +45,25 @@ class BookViewSet(ModelViewSet):
         if self.action == "retrieve":
             return serializers.BookDetailSerializer
         return serializers.BookSerializer
+
+    @action(
+        methods=["post"],
+        detail=True,
+        url_path="toggle-library",
+        url_name="toggle-library",
+    )
+    def toggle_library(self, request, pk=None):
+        book = self.get_object()
+        user = self.request.user
+
+        library = user.library.all()
+
+        if book in library:
+            user.library.remove(book)
+            return Response({"status": "Book was removed"})
+
+        user.library.add(book)
+        return Response({"status": "Book was added"})
 
 
 class ChapterViewSet(
