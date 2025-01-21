ce26e9feffe5ef266001de9272f3876a5535c94d
Maksym Nimych <maksimnimitch@gmail.com>
2025-01-21 22:47:48 +0200

    Register the model Commentary in the admin panel

--- book/admin.py
+++ book/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from book.models import Author, Book, Chapter, Genre
+from book.models import Author, Book, Chapter, Commentary, Genre
 
 
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Chapter)
+admin.site.register(Commentary)
